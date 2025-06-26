d8222280139d7064a2b0e470f2302a3ed19f1ccd
Md Armaan <armaanshaikhh786@gmail.com>
2025-06-26 14:28:52 +0530

    stackImplementUsinGStack

---
+++ Stack/StackUStack.java
@@ -0,0 +1,20 @@
+package Stack;
+import java .util.Stack;
+
+public class StackUStack {
+    public static void main(String args[]){
+        Stack <Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        
+        System.out.println(s.peek());
+        System.out.println(s.pop());
+         System.out.println(s.peek());
+         System.out.println(s.pop());
+          System.out.println(s.peek());
+          System.out.println(s.pop());
+        
+    }
+    
+}
