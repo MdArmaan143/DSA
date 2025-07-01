daab564cd9fef6e5a77819e065470d5d4532234b
Md Armaan <armaanshaikhh786@gmail.com>
2025-07-01 14:53:36 +0530

    spiralM

---
+++ practice/spiralMatrix.java
@@ -0,0 +1,51 @@
+package practice;
+
+public class spiralMatrix {
+    public static void spiralM(int arr[][]){
+        int sr = 0;
+        int er = arr.length-1;
+        int sc = 0;
+        int ec = arr[0].length-1;
+        while(sr<=er && sc<=ec){
+            //top ...row fix
+            for(int j = sc;j<=ec;j++){
+                System.out.print(arr[sr][j] + " ");
+            }
+            //right
+             for(int i = sr+1;i<=er;i++){
+                System.out.print(arr[i][ec]+ " ");
+            }
+            //bottom
+             for(int j = ec-1;j>=sc;j--){
+                if(sr==er){
+                    break;
+                }
+                System.out.print(arr[er][j]+ " ");
+            }
+            //left
+             for(int i = er-1;i>=sr+1;i--){
+                 if(sc==ec){
+                    break;
+                }
+                System.out.print(arr[i][sc]+ " ");
+            }
+            sr++;
+            sc++;
+            er--;
+            ec--;
+
+        } 
+
+    }
+    public static void main(String[] args) {
+        int arr[][] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+        };
+        spiralM(arr);
+        
+    }
+    
+}
