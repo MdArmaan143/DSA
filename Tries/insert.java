437a4a11477d1b0f082da939888b0dd8662cefc7
Md Armaan <armaanshaikhh786@gmail.com>
2025-09-13 20:11:35 +0530

    practice Tries

---
+++ Tries/insert.java
@@ -0,0 +1,49 @@
+package Tries;
+
+public class insert {
+    static class Node {
+        Node children[] = new Node[26];
+        boolean eow = false;
+
+        Node(){
+            for(int i = 0;i<children.length;i++){
+                children[i] = null;
+            }
+        }
+    
+        
+    }
+    public static Node root = new Node();
+
+    public static void insertW(String word){
+        Node curr = root;
+        for(int level = 0;level<word.length();level++){
+            int idx = word.charAt(level) - 'a';
+            if(curr.children[idx] ==null){
+                curr.children[idx] = new Node();
+            }
+            curr = curr.children[idx];
+        }
+        curr.eow = true;
+    }
+    public static boolean search(String key){
+          Node curr = root;
+        for(int level = 0;level<key.length();level++){
+            int idx = key.charAt(level) - 'a';
+            if(curr.children[idx] ==null){
+                return false;
+            }
+            curr = curr.children[idx];
+        }
+       return curr.eow == true;
+    }
+    public static void main(String args[]){
+        String words[] = {"the","a","there","their","any","thee"};
+        for(int i = 0;i<words.length;i++){
+            insertW(words[i]);
+        }
+        System.out.println(search("any"));
+        System.out.println(search("thore"));
+    }
+    
+}
